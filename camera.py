87cddbf380d226efb3fa83a14acf9ef2270798ca
lukasgartmair <lukas.gartmair@gmail.com>
2023-12-27 15:45:42 +0100

    added splitscreen test

--- camera.py
+++ camera.py
@@ -12,16 +12,13 @@ class Camera:
     def __init__(self):
 
         self.canvas = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.camera_1 = pygame.Rect(0,0,int(SCREEN_WIDTH),int(SCREEN_HEIGHT))
-        # self.camera_1 = pygame.Rect(0,0,int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2))
-        # self.camera_2 = pygame.Rect(int(SCREEN_WIDTH/2),0,int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2))
-        # self.camera_3 = pygame.Rect(0,int(SCREEN_HEIGHT/2),int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2))
-        # self.camera_4 = pygame.Rect(int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2),int(SCREEN_WIDTH/2),int(SCREEN_HEIGHT/2))
+        self.camera_1 = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT // 2)
+        self.camera_2 = pygame.Rect(0, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT // 2)
         
         self.sub1 = self.canvas.subsurface(self.camera_1)
-        # self.sub2 = self.canvas.subsurface(self.camera_2)
-        # self.sub3 = self.canvas.subsurface(self.camera_3)
-        # self.sub4 = self.canvas.subsurface(self.camera_4)
-        
+
     def get_subsurface_dimensions(self):
         return self.sub1.get_width(), self.sub1.get_height()
+    
+    def get_subsurface_center(self):
+        return self.sub1.center[0], self.sub1.center[1]
---
+++ splitscreen_test.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 14:50:46 2023
+
+@author: lukasgartmair
+"""
+
+import pygame
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+import sys
+class Camera:
+    def __init__(self, top, left, width, height):
+
+        self.canvas = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.camera = pygame.Rect(top, left, SCREEN_WIDTH, SCREEN_HEIGHT // 2)
+        self.subsurf = self.canvas.subsurface(self.camera)
+        
+    # def get_subsurface_dimensions(self, subsurface):
+    #     return self.subsurface.get_width(), self.subsurface.get_height()
+    
+    # def get_subsurface_topleft(self, subsurface):
+    #     return self.subsurface.get_width(), self.subsurface.get_height()
+        
+camera_1 = Camera(0,0,SCREEN_WIDTH, SCREEN_HEIGHT // 2)
+camera_2 = Camera(0,SCREEN_HEIGHT // 2,SCREEN_WIDTH, SCREEN_HEIGHT // 2)
+
+name = ""
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption(name)
+clock = pygame.time.Clock()
+
+bg = (0,0,255)
+bg2 = (0,255,0)
+
+def main () :
+    looping = True
+      
+    while looping :
+        pressed_keys = pygame.key.get_pressed()
+        filtered_events = []
+        try:
+            event_list = pygame.event.get()
+        except:
+            pass
+        for event in event_list:
+            quit_attempt = False
+            if event.type == pygame.QUIT:
+                quit_attempt = True
+            elif event.type == pygame.KEYDOWN:
+                alt_pressed = pressed_keys[pygame.K_LALT] or pressed_keys[pygame.K_RALT]
+                if event.key == pygame.K_ESCAPE:
+                    quit_attempt = True
+                elif event.key == pygame.K_F4 and alt_pressed:
+                    quit_attempt = True
+          
+            if quit_attempt:
+                pygame.quit()
+                sys.exit()
+            else:
+                filtered_events.append(event)
+                
+            screen.blit(camera_1.subsurf,camera_1.camera.topleft)
+            camera_1.subsurf.fill(bg)
+
+            screen.blit(camera_2.subsurf,camera_2.camera.topleft)
+            camera_2.subsurf.fill(bg2)
+            
+            pygame.display.update()
+            clock.tick(FPS)
+
+            # print(cam.get_subsurface_top_left(cam.sub1))
+            # screen.blit(cam.sub1,cam.get_subsurface_top_left(cam.sub1))
+            # cam.sub1.fill(bg)
+            # print(cam.get_subsurface_top_left(cam.sub2))
+            # sub2_rect = cam.sub2.get_rect()
+            # print(sub2_rect)
+            # sub2_rect.topleft =(0, SCREEN_HEIGHT // 2)
+            # print(sub2_rect)
+            # screen.blit(cam.sub2,sub2_rect.topleft)
+            # cam.sub2.fill(bg2)
+            # pygame.display.update()
+            # clock.tick(FPS)
+     
+main()
+
